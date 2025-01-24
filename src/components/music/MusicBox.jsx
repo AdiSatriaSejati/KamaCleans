5dcea0a4fed96aa84102f90bd133dc0a6dc1d40a
AdiSatria15_ <12221455@bsi.ac.id>
2025-01-24 22:12:28 +0700

    Update

--- src/components/music/MusicBox.jsx
+++ src/components/music/MusicBox.jsx
@@ -3,68 +3,24 @@ import './MusicBox.css';
 
 const MusicBox = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [audioContext, setAudioContext] = useState(null);
-  const [gainNode, setGainNode] = useState(null);
-  const [analyser, setAnalyser] = useState(null);
-  const audioRef = useRef(new Audio('../../../public/music/music_loop.mp3'));
-  const musicBarsRef = useRef(null);
-
-  useEffect(() => {
-    // Inisialisasi Audio Context
-    const initAudio = async () => {
-      const AudioContext = window.AudioContext || window.webkitAudioContext;
-      const context = new AudioContext();
-      const gain = context.createGain();
-      const audioAnalyser = context.createAnalyser();
-
-      gain.connect(audioAnalyser);
-      audioAnalyser.connect(context.destination);
-      gain.gain.value = 0.45;
-
-      setAudioContext(context);
-      setGainNode(gain);
-      setAnalyser(audioAnalyser);
-    };
-
-    initAudio();
-
-    return () => {
-      if (audioContext) {
-        audioContext.close();
-      }
-    };
-  }, []);
+  const audioRef = useRef(new Audio('music/music_loop.mp3'));
 
   useEffect(() => {
     const audio = audioRef.current;
 
-    // Event listener untuk autoplay
-    const handleCanPlayThrough = () => {
-      audio.play().catch((error) => {
-        console.error("Autoplay failed:", error);
-      });
-      setIsPlaying(true);
-    };
+    if (isPlaying) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
 
-    audio.addEventListener('canplaythrough', handleCanPlayThrough);
-
-    // Memastikan audio berhenti saat komponen dibongkar
     return () => {
-      audio.removeEventListener('canplaythrough', handleCanPlayThrough);
       audio.pause();
       audio.currentTime = 0; // Reset audio saat komponen dibongkar
     };
-  }, []);
+  }, [isPlaying]);
 
   const togglePlay = () => {
-    const audio = audioRef.current;
-    if (isPlaying) {
-      audio.pause();
-    } else {
-      audio.play().catch((error) => {
-        console.error("Play failed:", error);
-      });
-    }
     setIsPlaying((prev) => !prev);
   };
 
@@ -72,7 +28,6 @@ const MusicBox = () => {
     <div 
       className={`mhMusicBars ${isPlaying ? 'active' : ''}`} 
       onClick={togglePlay}
-      ref={musicBarsRef}
     >
       <span></span>
       <span></span>
