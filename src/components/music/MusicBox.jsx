850faf169a6b32f038054b143f41a5c829ea3e86
AdiSatria15_ <12221455@bsi.ac.id>
2025-01-25 00:00:24 +0700

    Update

--- src/components/music/MusicBox.jsx
+++ src/components/music/MusicBox.jsx
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './MusicBox.css';
 import canAutoplay from 'can-autoplay';
 
 const MusicBox = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const audio = new Audio('/music/music_loop.mp3'); // Ganti dengan path yang sesuai
+  const audioRef = useRef(new Audio('/music/music_loop.mp3')); // Pastikan path benar
 
   useEffect(() => {
+    const audio = audioRef.current;
+
+    // Set audio muted untuk autoplay
+    audio.muted = true;
+
+    // Cek apakah autoplay diizinkan
     const checkAutoplay = async () => {
       const { result } = await canAutoplay.audio({ muted: true });
       if (result) {
-        audio.muted = true; // Set audio muted untuk autoplay
         try {
           await audio.play();
           setIsPlaying(true);
@@ -24,13 +29,25 @@ const MusicBox = () => {
 
     checkAutoplay();
 
+    // Event listener untuk autoplay
+    const handleCanPlayThrough = () => {
+      if (!isPlaying) {
+        audio.play();
+        setIsPlaying(true);
+      }
+    };
+
+    audio.addEventListener('canplaythrough', handleCanPlayThrough);
+
     return () => {
+      audio.removeEventListener('canplaythrough', handleCanPlayThrough);
       audio.pause();
       audio.currentTime = 0; // Reset audio saat komponen dibongkar
     };
-  }, [audio]);
+  }, []);
 
   const togglePlay = () => {
+    const audio = audioRef.current;
     if (isPlaying) {
       audio.pause();
     } else {
