3c070d87d7004098b24a70631fa7d1987515d5db
AdiSatriaSejati <12221455@bsi.ac.id>
2025-02-23 06:01:51 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -24,5 +24,8 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'framer-motion']
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`
   }
 })
